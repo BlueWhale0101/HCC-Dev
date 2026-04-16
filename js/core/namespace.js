5f1abf2e412f0de4b31da50de463330f0e9b0437
BlueWhale <wesley.eller@gmail.com>
2026-04-16 13:09:27 +0930

    Add files via upload

---
+++ js/core/namespace.js
@@ -0,0 +1,5 @@
+window.HCC = window.HCC || {};
+window.HCC.core = window.HCC.core || {};
+window.HCC.constants = window.HCC.constants || {};
+window.HCC.utils = window.HCC.utils || {};
+window.HCC.meta = Object.assign({ appName: 'Household Command Center', appEnv: 'dev' }, window.HCC.meta || {});
